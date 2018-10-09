c7f5737515ef9153aaae5487f08197c9b5d4ebc5
Devin Rasmussen <devin.rasmussen@canopytax.com>
2018-10-09 10:56:55 -0600

    remove unused babel

--- webpack.config.js
+++ webpack.config.js
@@ -4,19 +4,9 @@ module.exports = {
   entry: './root-application.js',
   output: {
     filename: 'main.js',
+    chunkFilename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     // publicPath: '/dist/',
-  },
-  module: {
-    rules: [{
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env'],
-          plugins: ['@babel/plugin-syntax-dynamic-import']
-        }
-      }
-    }]
   },
   mode: 'development',
   devtool: 'source-map',
